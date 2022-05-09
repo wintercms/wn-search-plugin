317c6535291d11096e9e6d932a30764e44ebc600
Ben Thomson <git@alfreido.com>
2022-05-09 14:53:54 +0800

    Check class is using correct behavior before index/flush

--- console/FlushCommand.php
+++ console/FlushCommand.php
@@ -3,6 +3,8 @@
 namespace Winter\Search\Console;
 
 use Laravel\Scout\Console\FlushCommand as BaseFlushCommand;
+use Winter\Search\Behaviors\Halcyon\Searchable as HalcyonSearchable;
+use Winter\Search\Behaviors\Searchable;
 
 class FlushCommand extends BaseFlushCommand
 {
@@ -12,4 +14,33 @@ class FlushCommand extends BaseFlushCommand
      * @var string
      */
     protected $signature = 'search:flush {model}';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $class = $this->argument('model');
+
+        $model = new $class;
+
+        if (
+            !$model->isClassExtendedWith(Searchable::class)
+            && !$model->isClassExtendedWith(HalcyonSearchable::class)
+        ) {
+            $this->error(sprintf(
+                'Class %s does not implement the %s or the %s behavior',
+                $class,
+                Searchable::class,
+                HalyconSearchable::class,
+            ));
+            return 1;
+        }
+
+        $model::removeAllFromSearch();
+
+        $this->info('All ['.$class.'] records have been flushed.');
+    }
 }
--- console/ImportCommand.php
+++ console/ImportCommand.php
@@ -2,7 +2,10 @@
 
 namespace Winter\Search\Console;
 
+use Illuminate\Contracts\Events\Dispatcher;
 use Laravel\Scout\Console\ImportCommand as BaseImportCommand;
+use Winter\Search\Behaviors\Halcyon\Searchable as HalcyonSearchable;
+use Winter\Search\Behaviors\Searchable;
 
 class ImportCommand extends BaseImportCommand
 {
@@ -14,4 +17,42 @@ class ImportCommand extends BaseImportCommand
     protected $signature = 'search:import
             {model : Class name of model to bulk import}
             {--c|chunk= : The number of records to import at a time (Defaults to configuration value: `search.chunk.searchable`)}';
+
+    /**
+     * Execute the console command.
+     *
+     * @param  \Illuminate\Contracts\Events\Dispatcher  $events
+     * @return void
+     */
+    public function handle(Dispatcher $events)
+    {
+        $class = $this->argument('model');
+
+        $model = new $class;
+
+        if (
+            !$model->isClassExtendedWith(Searchable::class)
+            && !$model->isClassExtendedWith(HalcyonSearchable::class)
+        ) {
+            $this->error(sprintf(
+                'Class %s does not implement the %s or the %s behavior',
+                $class,
+                Searchable::class,
+                HalyconSearchable::class,
+            ));
+            return 1;
+        }
+
+        $events->listen(ModelsImported::class, function ($event) use ($class) {
+            $key = $event->models->last()->getScoutKey();
+
+            $this->line('<comment>Imported ['.$class.'] models up to ID:</comment> '.$key);
+        });
+
+        $model::makeAllSearchable($this->option('chunk'));
+
+        $events->forget(ModelsImported::class);
+
+        $this->info('All ['.$class.'] records have been imported.');
+    }
 }
